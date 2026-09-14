674a55e00591b79762efc79a022f21b244d16167
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2026-09-13 19:09:26 -0500

    polish: refine motion against transitions.dev token scale (#1)

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -89,7 +89,7 @@ export function PrimaryFeatures() {
                     <div
                       key={feature.title}
                       className={clsx(
-                        'group relative rounded-full py-1 px-4 lg:rounded-r-none lg:rounded-l-xl lg:p-6',
+                        'group relative rounded-full py-1 px-4 transition-colors duration-medium ease-smooth-out hover:duration-fast lg:rounded-r-none lg:rounded-l-xl lg:p-6',
                         selectedIndex === featureIndex
                           ? 'bg-white lg:bg-white/10 lg:ring-1 lg:ring-inset lg:ring-white/10'
                           : 'hover:bg-white/10 lg:hover:bg-white/5'
@@ -98,7 +98,7 @@ export function PrimaryFeatures() {
                       <h3>
                         <Tab
                           className={clsx(
-                            'font-display text-lg [&:not(:focus-visible)]:focus:outline-none',
+                            'font-display text-lg transition-colors duration-medium ease-smooth-out hover:duration-fast [&:not(:focus-visible)]:focus:outline-none',
                             selectedIndex === featureIndex
                               ? 'text-blue-600 lg:text-white'
                               : 'text-blue-100 hover:text-white lg:text-white'
@@ -110,7 +110,7 @@ export function PrimaryFeatures() {
                       </h3>
                       <p
                         className={clsx(
-                          'mt-2 hidden text-sm lg:block',
+                          'mt-2 hidden text-sm lg:block transition-colors duration-medium ease-smooth-out group-hover:duration-fast',
                           selectedIndex === featureIndex
                             ? 'text-white'
                             : 'text-blue-100 group-hover:text-white'
